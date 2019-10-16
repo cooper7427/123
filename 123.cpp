cf00d59768a27a81e5ffce675e69c80153dc812b
sokusheva <natasha.sokusheva@yandex.ru>
2019-10-16 20:59:12 +0500

    v4

--- 123.cpp
+++ 123.cpp
@@ -1,10 +1,20 @@
-#include <stdio.h>
+#include <iostream>
 
-int main (void)
-{
-   int a;
-   printf("Введите\n");
-   scanf("%d", &a);
-  printf("Hello, %d!\n", a);
-  return 0;
+using namespace std;
+
+int main(){
+   char str[100];
+   bool IsBreak=false;
+   cout<<"آâهنèٍه"<<endl;
+   cin>>str;
+   for (int i=0; str[i]!='\0'; i++)
+   if (str[i]<48 || str[i]>57) {
+      cout<<"آâهنهيî يه ÷èٌëî\n"; 
+      IsBreak=true; 
+      break;
+   }
+   if(IsBreak==false){
+      cout<<"Hello, "<<str<<endl;
+   }
+   return 0;
 }
