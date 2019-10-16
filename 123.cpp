5250b0b29dbde39f62937c7e0d48b665677f9fe3
sokusheva <natasha.sokusheva@yandex.ru>
2019-10-16 21:30:47 +0500

    v6

--- 123.cpp
+++ 123.cpp
@@ -3,18 +3,18 @@
 using namespace std;
 
 int main(){
-   char str[100];
-   bool IsBreak=false;
-   cout<<"آâهنèٍه"<<endl;
-   cin>>str;
-   for (int i=0; str[i]!='\0'; i++)
-   if (str[i]<48 || str[i]>57) {
-      cout<<"آâهنهيî يه ÷èٌëî\n"; 
-      IsBreak=true; 
-      break;
-   }
-   if(IsBreak==false){
-      cout<<"Hello, "<<str<<endl;
-   }
-   return 0;
+char str[100];
+bool IsBreak=false;
+cout«"Введите"«endl;
+cin»str;
+for (int i=0; str[i]!='\0'; i++)
+if (str[i]<48 || str[i]>57) {
+cout«"Введено не число\n";
+IsBreak=true;
+break;
+}
+if(IsBreak==false){
+cout«"Hello, "«str«"7"«endl;
+}
+return 0;
 }
